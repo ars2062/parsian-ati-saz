73b9cfb3b94741ec762844ea9a8e7d6b5c01c6ae
blackghost137 <mohk1379@gmail.com>
2019-05-02 00:49:09 +0430

    app structure and make actions  for login and verify code

---
+++ src/store/modules/account.js
@@ -0,0 +1,40 @@
+
+import { axios } from "axios";
+import consts from "../../consts";
+
+export default {
+    namespaced:true,
+    state:{
+        loading:false
+    },
+    getters:{
+
+    },
+    mutations:{},
+    actions:{
+        login:function({ commit } , {phone}){
+            axios.post(
+                consts.api_url+'account/login',{
+                    phone:phone
+                }
+            ).then(res => {
+                alert(res.data.message);
+            }).catch(r => {
+                alert('error');
+            });
+        },
+        verify:function ({commit},{phone,code}) {
+            axios.post(
+                consts.api_url+'account/verify',{
+                    phone:phone,
+                    code:code
+                }
+            ).then(res => {
+                alert(res.data.message);
+                localStorage.setItem('token',res.data.token);
+            }).catch(r => {
+                alert('error');
+            });
+        }
+    },
+}
