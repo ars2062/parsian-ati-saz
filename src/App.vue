d9d9bc2da7ceeb50eadbddf0a890df9032aea54a
Arshia Moghaddam <ars2062@gmail.com>
2019-05-03 15:46:16 +0430

    commit3

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
-    <Nav v-if="this.$route.path!=='/admin'"></Nav>
+    <Nav v-if="!this.$route.path.includes('admin')"></Nav>
     <router-view/>
   </div>
 </template>
