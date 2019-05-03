151afb636a7f97411fe748493c8d21a778b80ac6
Arshia Moghaddam <ars2062@gmail.com>
2019-05-03 15:52:22 +0430

    commit

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
     <Nav v-if="!this.$route.path.includes('admin')"></Nav>
     <router-view/>
-  </div>
+  </div> 
 </template>
 
 <script>
