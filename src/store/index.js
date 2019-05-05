b59ea37c51f4becfc5f73984bf82c80964a17cc8
Arshia Moghaddam <ars2062@gmail.com>
2019-05-05 07:58:10 +0430

    ممد!!!!!!!!!!!!!!!!!!!!!

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import Vuex from "vuex";
 import contact from "./modules/contact";
 import adviser from "./modules/adviser";
-import modal from "./modules/modal";
 
 Vue.use(Vuex);
 
@@ -11,8 +10,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   modules:{
     contact:contact,
-    adviser:adviser,
-    modal:modal
+    adviser:adviser
   },
   state:{
       authenticated:false,
