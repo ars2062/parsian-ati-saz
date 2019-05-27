525f0e749a12263aedcff931064dca5c3e5da5dc
Arshia Moghaddam <ars2062@gmail.com>
2019-05-27 20:55:13 +0430

    commit

--- src/router.js
+++ src/router.js
@@ -22,7 +22,8 @@ Vue.use(Router);
 let router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
-  routes: [{
+  routes: [
+    {
       path: "/",
       name: "home",
       component: Home
